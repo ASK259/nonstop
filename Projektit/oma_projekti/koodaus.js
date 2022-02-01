55d4e657828433dd7c43697e431aad7e6cb80249
ASK259 <antti.kuusisto@edu.keuda.fi>
2022-02-01 15:05:18 +0200

    Askarreltu lisää.

--- Projektit/oma_projekti/koodaus.js
+++ Projektit/oma_projekti/koodaus.js
@@ -1,11 +1,11 @@
 //const obj = new LuoTieto1(mer,mal,tun,ge,nop,ak1,ak2,pin,crW,tDi);
 /*window.onload = function() {
-  document.getElementById("laskuri").onsubmit = tuoTie;
+  document.getElementById("laske").onclick = tuoTie;
   document.getElementById("laske").onclick = tuNayt;
   document.getElementById("laske").onclick = tarkistaKen;
   document.getElementById("laTu").onclick = valTu;
   document.getElementById("poista").onclick = poTu;
-  document.getElementById("tuSyAr").textContent = obj.kirTie;
+  //document.getElementById("tuSyAr").textContent = obj.kirTie;
 }*/
 
 const test = tu => console.log(tu);
@@ -64,7 +64,7 @@ function tuoTie() {
   let alkNop = maAlkpNop(tire,vaihKokval);
   let nop = noKiLu(alkNop,rpmC,rpmS,rpmE);
   const obj = new LuoTieto1(mer,mal,tun,ge,nop,ak1,ak2,pin,crW,tDi);
-  //document.getElementById("tuSyAr").textContent = obj.kirTie;
+  document.getElementById("tuSyAr").textContent = obj.kirTie;
   let key = document.getElementById("tunnus").value;
   window.localStorage.setItem(tun,JSON.stringify(obj));
   alert(`Tuloksesi: ${obj.kirTie}.`);
@@ -78,7 +78,7 @@ const noKiLu = (alN,rpmC,rpmS,rpmE) => {
   return nop;
 }
 
-function tuNayt() {
+/*function tuNayt() {
   let key = document.getElementById("tunnus").value;
   console.log(key);
   let ha = window.localStorage.getItem(key);
@@ -86,7 +86,7 @@ function tuNayt() {
   let ti = JSON.parse(ha);
   console.log(ti);
   document.getElementById("tuSyAr").textContent = ti.kirTie;
-}
+}*/
 
 const aksSuhdLu = (ak1,ak2) => ak2 / ak1;
 
@@ -114,15 +114,71 @@ function poTu() {
   alert("Tietosi on poistettu!");
 }
 
-function tarkistaKen() {
-
+function validateForm() {
+  let mer = document.getElementById("merkki").value;
+  let mal = document.getElementById("malli").value;
+  let tun = document.getElementById("tunnus").value;
+  let rpmC = document.getElementById("rpmC").value;
+  let rpmS = document.getElementById("rpmS").value;
+  let rpmE = document.getElementById("rpmE").value;
+  let ge = document.getElementById("gear").value;
+  let tDi = document.getElementById("tDia").value;
+  let ak1 = document.getElementById("aks1").value;
+  let ak2 = document.getElementById("aks2").value;
+  let pin = document.getElementById("pinion").value;
+  let crW = document.getElementById("crownW").value;
+  const tark = (arv) => (arv.length == 4) ? true : false; // kierrosluvun pituuden tarkistus funktio
+  if (mer.length > 20 || /\d/ig.test(mer) == true || mer.length == 0 ) {
+    alert(`Syötä merkki!`);
+    laskuri.merkki.focus();
+    return (false);
+  } else if (mal.length > 20 || mal.length == 0) {
+    alert(`Syötä malli!`);
+    laskuri.malli.focus();
+    return (false);
+  } else if (tun.length > 15 || tun.length == 0) {
+    alert(`Syötä tunnus!`);
+    laskuri.tunnus.focus();
+    return (false);
+  } else if (rpmC <= 4 || /\D/ig.test(rpmC) == true) {
+    alert(`Syötä kierrosluvun nousuväli!`);
+    laskuri.rpmC.focus();
+    return (false);
+  } else if (tark(rpmS) == false || /\D/ig.test(rpmS) == true) {
+    alert(`Syötä aloitus kierrosluku!`);
+    laskuri.rpmS.focus();
+    return (false);
+  } else if (tark(rpmE) == false || /\D/ig.test(rpmE) == true) {
+    alert(`Syötä kierrosluvun päättymispiste!`);
+    laskuri.rpmE.focus();
+    return (false);
+  } else if (ge.length != 1 || isNaN(ge)) {
+    alert(`Syötä vaihde!`);
+    laskuri.gear.focus();
+    return (false);
+  } else if (tDi.length > 4 || tDi.length == 0 || /\D/ig.test(tDi) == true) {
+    alert(`Syötä renkaanhalkaisia!`);
+    laskuri.tDia.focus();
+    return (false);
+  } else if (ak1.length > 3 || ak1.length == 0 || /\D/ig.test(ak1) == true){
+    alert(`Syötä 1. akselinhammasluku!`);
+    laskuri.aks1.focus();
+    return (false);
+  } else if (ak2.length > 3 || ak2.length == 0 || /\D/ig.test(ak2) == true) {
+    alert(`Syötä 2. akselinhammasluku!`);
+    laskuri.aks2.focus();
+    return (false);
+  } else if (pin.length > 2 || pin.length == 0 || /\D/ig.test(pin) == true) {
+    alert(`Syötä pienenlautaspyöränhammasluku!`);
+    laskuri.pinion.focus();
+    return (false);
+  } else if (crW.length > 3 || crW.length == 0 || /\D/ig.test(crW) == true) {
+    alert(`Syötä isonlautaspyöränhammasluku!`);
+    laskuri.crownW.focus();
+    return (false);
+  }
 }
 
-window.onload = function() {
-  document.getElementById("laskuri").onsubmit = tuoTie;
-  document.getElementById("laske").onclick = tuNayt;
+/*window.onload = function() {
   document.getElementById("laske").onclick = tarkistaKen;
-  document.getElementById("laTu").onclick = valTu;
-  document.getElementById("poista").onclick = poTu;
-  document.getElementById("tuSyAr").textContent = obj.kirTie;
-}
+}*/
